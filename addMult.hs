87e5123d20b5cafb7a7ec57a7fa6cdb9be685e67
Hammad Asad <hammad_asad@msn.com>
2015-09-10 11:10:51 -0400

    Update addMult.hs

--- addMult.hs
+++ addMult.hs
@@ -1,6 +1,10 @@
+-- Addition and Multiplication in Haskell
+-- Use of only pre and suc functions, == comparison, recursion and if-then-else statements
+
+-- Test
 -- \x -> \y -> x >> y :: Monad m => m a -> m b -> m b
 
---
+-- Random method
 m n = if n > 100
         then n - 10
         else m(n + 11)
@@ -9,12 +13,15 @@ m n = if n > 100
 -- m(35) = 91
 -- m(88) = 100
 
--- 
+-- Helper functions
 pre n = n - 1
 suc n = n + 1
+
+--Add function
 add x y = if y == 0
              then x
              else add (suc x) (pre y)
+--Mult function , uses the Add function
 mult x y = if y == 1
              then x
 	         else add x (mult x (pre y))
